f0b7a21976455867f778b63538873db7968a3a84
Tejasbisen786 <bisentejas@gmail.com>
2023-07-07 01:29:24 +0530

    intial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// variable and Constant
+
+// const acccount_id= 123456
+// console.log(acccount_id) 
+// let accountEmail='bisentejas@gmail.com'
+// console.log(accountEmail)
+// var accountpassword='Tejas@123'
+// console.log(accountpassword)
+
+let accountEmail="bisentejas@gmail.com"
+let accountpassword="Tejas#123"
+let accountCity ="Gondia"
+
+console.log([accountCity,accountEmail,accountpassword])
