e68988b0544d3b0b3216e5b4415b953f7a8c7536
Tejasbisen786 <bisentejas@gmail.com>
2023-07-08 22:15:38 +0530

    file-name-updates

---
+++ Lecture 1 To 6 .js
@@ -0,0 +1,140 @@
+//  ***************variable and Constant****************
+
+// const acccount_id= 123456
+// console.log(acccount_id)
+// let accountEmail='bisentejas@gmail.com'
+// console.log(accountEmail)
+// var accountpassword='Tejas@123'
+// console.log(accountpassword)
+
+// let accountEmail="bisentejas@gmail.com"
+// let accountpassword="Tejas#123"
+// let accountCity ="Gondia"
+
+// console.log([accountCity,accountEmail,accountpassword])
+
+// ***************Datatype And Ecmastandards***********************
+
+//  let user_Name="Tejas Bisen"
+//  let x=true
+//  let num=144343
+//  let deci=90003.000
+//  let v;
+// let n =0
+//  console.log([typeof user_Name , typeof x , typeof num , typeof deci ,typeof v, typeof n])
+
+//  Remember
+// Type of null is always Object
+// Type of undefined is undefined
+
+// *************** Datatype Conversion ************************
+
+// let age;
+// let score =33;
+// console.log (typeof score)
+// let s="33"
+
+// let valuenumber =Number(s)
+// console.log(valuenumber)
+// console.log(typeof valuenumber)
+// let islogged =1
+// let converted =Boolean(islogged)
+// console.log(typeof converted)
+// console.log(typeof islogged)
+
+// let num =45
+// let strnum = String(num)
+// console.log(typeof strnum)
+
+
+// ***************** Lecture 4 : String to Number Conversion ********************
+
+ //Operation Conversion 
+// let value = 3
+// let negvalue= -value
+// console.log(negvalue)
+
+//basic Opeartion 
+// console.log(5+5)
+// console.log(5-5)
+// console.log(5*5)
+// console.log(5/5)
+// console.log(5**5)
+// console.log(2%3)
+
+//Conacataion String using " + "
+
+// let str1="Tejas"
+// let str2 ="Bisen"
+
+// let str3=str1+str2
+// console.log(str3)
+
+
+// Note : Use Paranthesis to make good calcluation without making complicated 
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+// Tricky Conversion 
+
+// console.log(+ true)
+// console.log(+" ");
+
+
+
+
+// Assignment Opearators
+// let num1,num2,num3,
+//  num = num2=num3=2+2
+// console.log(num)
+
+// let  gamecount=100
+
+// gamecount++
+// console.log(gamecount);
+
+
+//PRE AND POST increment 
+
+// let Gcount=100
+// Gcount++
+// console.log(Gcount);
+
+// ++Gcount
+// console.log(Gcount);
+
+
+// **************Lecture 5 : Comparation of Datatype ************************
+
+// console.log(2>1)  Greter than
+// console.log(2>=1); gretaer than equal to
+// console.log(2<1);   less than
+// console.log(2<=1); less than eqaul to 
+// console.log(2!=1); not equal to
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+// Comparasions Convert Null to a Number Treating 0. Thaat's Why (3) num>=0 is true AND (1)  Null>0 is false  
+
+
+// Strict Check ===
+// console.log("2"===2);
+
+
+// *********************Lecture 6: Data-Type Of Javascript ******************
+// const score =100
+// const scoreValue=100.3
+// const isloggedin = false
+// const outsideTemp = null
+// let userEmail;
+
+// console.log(typeof score);
+// console.log(typeof scoreValue);
+
+
+
+
