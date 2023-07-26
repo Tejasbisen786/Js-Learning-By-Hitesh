4b14e9f7959a107a5e855b53412bd750c46a2e40
Tejasbisen786 <bisentejas@gmail.com>
2023-07-26 23:38:53 +0530

    Object Method Updates

--- Lecture-13.js
+++ Lecture-13.js
@@ -9,35 +9,46 @@
 
 const tUser={
     nameof:"Tejas",
+    fullNames:"Tejas Wasudeo Bisen",
     age:18,
     location:"GOndia",
     email:"bisentejas@gmail.com",
     isLoggedin: false,
-    last_Login_Days:["Monday","Sunday"]
+    last_Login_Days:["Monday","Sunday"],
+    // [mysum]: "mykey"   
 
 }
+
+const mysum=Symbol("key")
+// console.log(typeof mysum);
+
+// Accessing the Objects 
+// console.log(tUser["email"]);
+// console.log(tUser["fullNames"]);
+// console.log(tUser["mysum"]); // string type but we want symobol type
 
 // console.log(tUser.age)
 // console.log(tUser.last_Login_Days)
 // console.log(tUser.nameof)
 
-const new_user={
-    "FullName":"Tejas Wasudeo Bisen"
-}
-// console.log(new_user.FullName);
-//Aceess Element
-// console.log(new_user.FullName);
+//symbol DataType
+// const new_user={
+//     "FullName":"Tejas Wasudeo Bisen" 
+// }
+// // console.log(new_user.FullName);
+// //Aceess Element
+// // console.log(new_user.FullName);
 
-// Defining Symbol And do add into array and five me access into the Array
+// // Defining Symbol And do add into array and five me access into the Array
 
-// Defining Symbol
-const mysum=Symbol("key")
+// // Defining Symbol
+// const mysum=Symbol("key")
 // a doing symbol into array
 
-const new_users={
-    [mysum]:"myKey"
+// const new_users={
+//     [mysum]:"myKey"
 
-}
+// }
 
 //Acessing 
 // console.log( typeof new_users[mysum]);
@@ -60,11 +71,36 @@ const new_users={
 
 //Function Inside Objects
 
-new_user.greeting=function()
+// new_user.greeting=function()
+// {
+
+//     console.log("Good Morning");
+// }
+// new_user.greeting()
+
+
+ 
+
+// tUser.nameof="Tejas Bisen Now"
+
+// console.log(tUser);
+// tUser.nameof="Tejas Bisen"
+// console.log(tUser);
+
+ //restrict To change the value in obejct by using freeze method 
+// Object.freeze(tUser)
+
+tUser.nameof="Tushar Bisen"
+
+// console.log(tUser);
+// Anonymus Function 
+tUser.greeting=function()
 {
-
-    console.log("Good Morning");
+    console.log("Hello Js User");
 }
-new_user.greeting()
-
-
+tUser.greeting2=function()
+{
+    console.log(`helloe my name is ${this.nameof}`);
+}
+console.log(tUser.greeting());
+console.log(tUser.greeting2());
