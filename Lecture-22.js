79d2c0cd23c36e3208281bde3145015f0ee4073e
Tejasbisen786 <bisentejas@gmail.com>
2023-07-30 23:11:42 +0530

    Lecture 22 Added

---
+++ Lecture-22.js
@@ -0,0 +1,9 @@
+// ***** Lecture-22 : How does javascript Execute Code + Stack Code *******
+
+// Javascript Execution Context 
+//  1) Global EC  :- kept in this (node)  in browser (window)
+// 
+
+
+
+// ****** CALL STACK *****
