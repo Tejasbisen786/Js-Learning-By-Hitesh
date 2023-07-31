14a517e3dbe18b9ab62f61ac88a945135fcb4807
Tejasbisen786 <bisentejas@gmail.com>
2023-07-31 23:37:57 +0530

    Lecture 25 Added

---
+++ Lecture-25.js
@@ -0,0 +1,34 @@
+// ****Lecture 25: 28 While and do while loop in Javascript *****
+
+// While Loop And Do while Loop 
+
+// While Loop 
+// let x=0;
+// while (x<=10) {
+//     // console.log(`Value of x is : ${x}`);
+//     x=x+2;
+// }
+
+
+
+// using array 
+let myarray=["Tejas","tushar","Bisen"]
+
+// let arr=0;
+// while(arr<myarray.length)
+// {
+//     console.log(myarray[arr]);
+//     arr=arr+1;
+// }
+
+
+
+// dowhile Loop 
+
+
+let score=11
+do {
+    
+    console.log(`Score is ${score}`)
+    score++
+} while (score<=10)
