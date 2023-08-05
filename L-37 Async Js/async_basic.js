54ba3d050fb65b027ed1b97a7c70d33d97237e27
Tejasbisen786 <bisentejas@gmail.com>
2023-08-06 00:34:45 +0530

    Lecture- 37 Added

---
+++ L-37 Async Js/async_basic.js
@@ -0,0 +1,19 @@
+// javascript 
+// async
+
+// JavAscript : Synchronous 
+//            : Single Threaded
+
+// -> Execution  Context 
+    //  |->  excute one line of code at a time 
+
+            // console.log(1)
+            // console.log(1)  
+            // each operation waits for the last one to complete before executing 
+
+    // blocking code : flow break -> Read file Sync
+    //  No Blocking Code  : non blocking -> Read File Async
+
+      
+
+
