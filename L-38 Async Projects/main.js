7bf7481f5314d9d855e878be33667de335b7b908
Tejasbisen786 <bisentejas@gmail.com>
2023-08-06 12:55:52 +0530

    Lecture 38 added with home Homework

---
+++ L-38 Async Projects/main.js
@@ -0,0 +1,62 @@
+// ***setTimeout Function
+// const printMe= function(){
+//     console.log("Tejas Bisen");
+// }
+// const interval = setTimeout(printMe,2000)
+
+// document.querySelector('#stop').addEventListener('click',function(){
+//     clearInterval(interval)
+
+// })
+
+// const start = document.querySelector("#start");
+// const stop = document.querySelector("#start");
+
+// const h1 = document.querySelector("h2");
+// const changeName = function () {
+//   h1.innerHTML = `<h2>Chai And Code </h2>`;
+// };
+
+// const h2chnage=setTimeout(changeName, 2000);
+
+// setTimeout
+// document.querySelector('#stop').addEventListener('click',function(){
+//     clearInterval(h2chnage)
+
+// })
+
+ // clearInterval 
+// button.addEventListener("click", function () {
+    // console.log("Stopped");
+//   setTimeout(changeName, 2000);
+// });
+
+
+// ** Setinterval Function 
+
+
+// const start = document.querySelector("#start");
+// const stopbut = document.querySelector("#stop");
+// let interval3;
+
+// const sayDate =function(){
+//     console.log(Math.floor(Math.random()*10+1));
+// }
+
+
+// ****************HoMEWORK************************
+
+// **Starting The Interval By using buttion
+
+// start.addEventListener('click',function(){
+//     interval3= setInterval(sayDate,1000)
+    
+// })
+ 
+// **Stopping The Interval By using buttion
+
+//  stopbut.addEventListener('click',function(){
+ 
+//      clearInterval(interval3)
+//  })
+  
